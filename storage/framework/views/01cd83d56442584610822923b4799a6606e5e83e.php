003edf2e612f8295b61a9a77dfcec0f7a201f180
penzes98 <32952776+penzes98@users.noreply.github.com>
2022-05-01 19:54:15 +0200

    Add files via upload
    login

---
+++ storage/framework/views/01cd83d56442584610822923b4799a6606e5e83e.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title><?php echo $__env->yieldContent('title', $title); ?></title>
+</head>
+<body>
+    <div class="container">
+        <h1><?php echo $__env->yieldContent('title', $title); ?></h1>
+        <?php echo $__env->yieldContent('content'); ?>
+    </div>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\doodleLike\resources\views/app.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8dc549ef38eda20b2aebe0e15591049d7bc04f47.php
@@ -0,0 +1,31 @@
+<?php $__env->startSection('content'); ?>
+    <div class="row">
+        <div class="col-md-6">
+            <?php if(session('success')): ?>
+                <p class="alert alert-success"><?php echo e(session('success')); ?></p>
+            <?php endif; ?>
+            <?php if($errors->any()): ?>
+                <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $err): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <p class="alert alert-danger"><?php echo e($err); ?></p>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            <?php endif; ?>
+            <form action="<?php echo e(route('login.action')); ?>" method="POST">
+                <?php echo csrf_field(); ?>
+                <div class="mb-3">
+                    <label>Username <span class="text-danger">*</span></label>
+                    <input class="form-control" type="username" name="username" value="<?php echo e(old('username')); ?>" />
+                </div>
+                <div class="mb-3">
+                    <label>Password <span class="text-danger">*</span></label>
+                    <input class="form-control" type="password" name="password" />
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary">Login</button>
+                    <a class="btn btn-danger" href="<?php echo e(route('home')); ?>">Back</a>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\doodleLike\resources\views/user/login.blade.php ENDPATH**/ ?>
