22fd4c8645a9408a6dd58c95f698a8ad1a851325
Attila Bátori <71729757+batoriattila@users.noreply.github.com>
2022-05-24 22:54:49 +0200

    -megmaradt angol üzenetek/hibaüzenetek átírása magyarra -elfelejtett jelszó oldalon laravel logó a sajátra cserélése

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,12 +2,35 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <div class="logo_">
+                    <h4><i class="fa fa-calendar"></i>|Doodle</h4>
+                </div>
             </a>
         </x-slot>
 
+        <style>
+            .logo_{
+                color:#F64C72;
+                font-family: 'Bebas Neue';
+                /*font-family: 'Source Sans Pro', sans-serif;*/
+                letter-spacing: 11px;
+                font-size: 4rem;
+                /*margin: 0.5rem;*/
+                align-content: center;
+                alignment: center;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                max-height: 11rem;
+                /*border: 10px;*/
+            }
+        </style>
+
+        <link href='https://fonts.googleapis.com/css?family=Bebas Neue' rel='stylesheet'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Elfelejtetted a jelszót? Semmi probléma. Csak add meg az e-mail címed és mi küldünk egy linket a jelszó visszaállításához.') }}
         </div>
 
         <!-- Session Status -->
@@ -28,7 +51,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Jelszó visszaállító link küldése.') }}
                 </x-button>
             </div>
         </form>
